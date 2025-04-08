615c4016eff6ab20ce00703c443204a81759be55
Thu Thao <nguyenthithuthao071203@gmail.com>
2025-04-08 17:49:21 +0700

    css chua xong font

--- resources/views/clients/layouts/header.blade.php
+++ resources/views/clients/layouts/header.blade.php
@@ -12,4 +12,8 @@
   <link href="{{ asset('frontend/css/map.css') }}" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap" rel="stylesheet">
 </head>
